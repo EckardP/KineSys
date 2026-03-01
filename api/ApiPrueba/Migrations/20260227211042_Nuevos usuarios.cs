854db5024b43f8d29c53b5a4dd35754c1b4e7f3c
adavidrodrii <adavidrodrii@gmail.com>
2026-02-28 21:01:40 -0500

    Nuevos usuarios

---
+++ api/ApiPrueba/Migrations/20260227211042_Nuevos usuarios.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiPrueba.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nuevosusuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- api/ApiPrueba/Models/Rol.cs
+++ api/ApiPrueba/Models/Rol.cs
@@ -5,5 +5,8 @@
         Paciente =1,
         Terapeuta = 2,
         Administrador = 3,
+        Recepcionista = 4,
+        Contador = 5,
+        Supervisor = 6,
     }
 }
